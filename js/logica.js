46d01ba41a209d6c98a68361acd6fc592a1453b7
Ronalgon <al330056@edu.uaa.mx>
2024-03-08 18:40:27 -0600

    Portada, Sonidos y ya
    nomas pues que esperabas

--- js/logica.js
+++ js/logica.js
@@ -1,9 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Inicio</title>
-</head>
-<body>
-</body>
-</html>
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("button").forEach(function (button) {
+    button.addEventListener("click", function () {
+      var sonidoBoton = document.getElementById("sonidoBoton");
+      sonidoBoton.play();
+    });
+  });
+
+  function toggleSonido() {
+    var miAudio = document.getElementById("miAudio");
+    var botonSonido = document.getElementById("botonSonido");
+    if (miAudio.paused) {
+      miAudio.play();
+      botonSonido.innerHTML = "<i class='fa-solid fa-volume-high'></i>";
+      botonSonido.style.backgroundColor = "hsl(245, 98%, 77%)";
+    } else {
+      miAudio.pause();
+      botonSonido.innerHTML = "<i class='fa-solid fa-volume-xmark'></i>";
+      botonSonido.style.backgroundColor = "hsl(0, 90%, 67%)";
+    }
+  }
+
+  botonSonido.addEventListener("click", toggleSonido);
+});
