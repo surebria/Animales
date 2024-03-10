f4ee34134a054d36a018f8da2a4a33668af4d426
Valeria <ppollux.velas@gmail.com>
2024-03-09 20:18:38 -0600

    finalizacion de creditos

--- js/creditos.js
+++ js/creditos.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtener el canvas y el contexto
     var canvas = document.getElementById("miCanvas");
     var ctx = canvas.getContext("2d");
 
-    // Crear una nueva imagen
     var img = new Image();
     img.onload = function () {
-        // Dibujar la imagen en el canvas
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     };
 
-    // Establecer la ruta de la imagen de fondo
     img.src = "../img/Extras/border.png";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var fechaActual = new Date().toLocaleDateString();
+    var fechaElement = document.querySelector('.date');
+    fechaElement.textContent = 'Fecha: ' + fechaActual;
+});
+
