57da4bac0296e7ee8ea62ec17e6778dec32fc752
JuanDluna <pablo.dluna@hotmail.com>
2024-03-09 23:26:41 -0600

    Juego casí acabado
    Ya se hace la 2da pantalla, falta finalización

--- js/juego.js
+++ js/juego.js
@@ -14,6 +14,11 @@ const arrayObjetos = [
 let puntaje = 0;
 let seconds = 0;
 let timerInterval;
+// Obtiene los jugadores locales 
+let usersLocal = JSON.parse(localStorage.getItem('usuarios'));
+// Obtiene el jugador actual filtrando por nombre
+let player = usersLocal.find(jugador => sessionStorage.getItem('jugador') === jugador.usuario);
+// Imposible que retorne un null
 
 const shuffleArray = (array) => {
     const shuffledArray = [...array];
@@ -29,10 +34,9 @@ const primerosTres = arrayObjetosShuffled.slice(0, 3);
 const siguientesTres = arrayObjetosShuffled.slice(3, 6);
 
 var animalesMostrados = () => {
-
-    //TODO Aquí va a estar una conexión con el localStorage para saber en qué nivel se encuentra el jugador
-    //niveleEjecutados = localStorage.getItem('nivelesEjecutado');
-    return nivelEjecutado === 1 ? primerosTres : siguientesTres;
+    // Si es 1 es por que acabo el primer nivel, se pasa al segundo
+    // En caso de que sea 0 o 2 es por que no ha jugado o ya jugo y presenta a hacerlo de nuevo
+    return player.progreso === 1 ? siguientesTres : primerosTres;
 };
 
 
@@ -42,54 +46,70 @@ var animalesMostrados = () => {
 
 //TODO Descomentar todo esto cuando la conexión a localStorage esté lista
 //function asignarImagenesAnimales(animalesMostrados)
-function asignarImagenesAnimales(primerosTres) { //TODO Modificar aquí cuando sea la segunda ejecución
-    var imagenes = document.querySelectorAll('#containerAnimales > section > img');
+function asignarImagenesAnimales(TresAnimales) { //TODO Modificar aquí cuando sea la segunda ejecución
+    var imagenes = document.querySelectorAll('#containerAnimales > img');
 
     imagenes.forEach((imagen, index) => {
-        imagen.src = primerosTres[index].animal;
-        imagen.dataset.casa = primerosTres[index].casa; // Asignar el nombre de la casa al atributo de datos
-        imagen.dataset.sonido = primerosTres[index].sonido;
+        imagen.src = TresAnimales[index].animal;
+        imagen.dataset.casa = TresAnimales[index].casa; // Asignar el nombre de la casa al atributo de datos
+        imagen.dataset.sonido = TresAnimales[index].sonido;
     });
 }
 
 function iniciar() {
 
-    // console.log(primerosTres);
-    var fondosSonidos = shuffleArray(primerosTres.map(objeto => {
+    if (player.progreso === 1) {
+        // Si el progreso es 1, no termino el 2do nivel, se establece el puntaje y tiempo a lo guardado
+        puntaje = player.ultPuntaje;
+        seconds = player.mejorTiempo;
+    }
+    var fondosSonidos = shuffleArray(animalesMostrados().map(objeto => {
         return {
             casa: objeto.casa,
             sonido: objeto.sonido
         };
-    })); //TODO También modificar aquí en segunda ejecución
-    //shuffleArray(animalesMostrados / / / )
-
+    }));
 
     var lienzos = document.querySelectorAll('canvas');
-    lienzos.forEach((soltar, index) => {
-        // console.log(soltar);
+    lienzos.forEach((canvas, index) => {
+
         var fondo = new Image();
+        var lienzo = canvas.getContext('2d');
+        lienzo.clearRect(0, 0, canvas.width, canvas.height);
 
         fondo.src = fondosSonidos[index].casa;
-        // console.log(fondo.src);
-        soltar.dataset.casa = fondosSonidos[index].casa; // Asignar el nombre de la casa al atributo de datos
-        soltar.dataset.sonido = fondosSonidos[index].sonido;
+        canvas.dataset.casa = fondosSonidos[index].casa;
+        canvas.dataset.sonido = fondosSonidos[index].sonido;
 
+        console.log(canvas);
 
         fondo.onload = () => {
-            lienzo.drawImage(fondo, 0, 0, soltar.width, soltar.height);
+            lienzo.drawImage(fondo, 0, 0, canvas.width, canvas.height);
         }
 
-        var imagenes = document.querySelectorAll('#containerAnimales > section > img');
-        for (var i = 0; i < imagenes.length; i++) {
-            imagenes[i].addEventListener('dragstart', arrastrado, false);
-            imagenes[i].addEventListener('dragend', finalizado, false);
+        var imagenes = document.querySelectorAll('#containerAnimales > img');
+        if (imagenes.length > 0) {
+            // Si existe la variable "imagenes"
+            // Es el primer juego, por lo que se asignan los eventos a los animales
+            for (var i = 0; i < imagenes.length; i++) {
+                imagenes[i].addEventListener('dragstart', arrastrado, false);
+                imagenes[i].addEventListener('dragend', finalizado, false);
+            }
+        } else {
+            // Si no existe la variable "imagenes"
+            // Es el segundo juego, por lo que se crean los elementos de img y se les asignan los eventos
+            for (var i = 0; i < 3; i++) {
+                document.getElementById('containerAnimales').appendChild(document.createElement('img'));
+                document.getElementById('containerAnimales').lastChild.addEventListener('dragstart', arrastrado, false);
+                document.getElementById('containerAnimales').lastChild.addEventListener('dragend', finalizado, false);
+                document.getElementById('containerAnimales').lastChild.dataset.casa = fondosSonidos[index].casa;;
+            }
         }
 
-        var lienzo = soltar.getContext('2d');
-        soltar.addEventListener('dragenter', eventoEnter, false);
-        soltar.addEventListener('dragover', eventoOver, false);
+        canvas.addEventListener('dragenter', eventoEnter, false);
+        canvas.addEventListener('dragover', eventoOver, false);
 
-        soltar.addEventListener('drop', (e) => soltado(e, lienzo, soltar), false);
+        canvas.addEventListener('drop', (e) => soltado(e, lienzo, canvas), false);
     });
 }
 
@@ -114,36 +134,78 @@ function arrastrado(e) {
     e.dataTransfer.setDragImage(e.target, e.target.width / 2, e.target.height / 2);
 }
 
-function soltado(e, lienzo, soltar) {
+function soltado(e, lienzo, canvas) {
     e.preventDefault();
     var id = e.dataTransfer.getData('Text');
     var elemento = document.getElementById(id);
-    var posX = e.pageX - soltar.offsetLeft;
-    var posY = e.pageY - soltar.offsetTop;
-    var casaAnimal = elemento.dataset.casa;
-    var casaCanvas = soltar.dataset.casa;
+    var posX = e.pageX;
+    var posY = e.pageY;
+    var casaAnimal = canvas.dataset.casa;
+    var casaCanvas = canvas.dataset.casa;
     var divPuntaje = document.getElementById('puntaje');
 
     if (casaAnimal === casaCanvas) {
         // El animal se arrastró a la casa correcta
-        lienzo.drawImage(elemento, posX, posY);
-        elemento.draggable = false;
-        elemento.style.visibility = 'hidden';
+        // Se elimina del div padre para dejarlo sin hijos
+        // Jaja, sin hijos, se quedo sin webos
+        elemento.remove();
+
+        lienzo.drawImage(elemento, posX, posY, 100, 100);
+
         // Se dibuja en el lienzo y se oculta del origen
         puntaje += Math.round(Math.floor(Math.random() * 1000) / seconds);
         // Se eleva el puntaje
 
         // Se ejecuta el audio indicando que es el animal correcto
-        var audio = new Audio(soltar.dataset.sonido);
+        var audio = new Audio(canvas.dataset.sonido);
         audio.play();
+
+
+        // Si no tiene hijos el container significa que arrastro todos los que debía
+        if (document.getElementById('containerAnimales').children.length === 0) {
+            if (player.progreso !== 1) {
+                usersLocal.forEach(jugador => {
+                    // Busca al jugador actual y le asigna el progreso 1
+                    if (jugador.usuario == player.usuario) {
+                        // Si el progreso es diferente de 1  (0 o 2) significa que ya paso el primer nivel, se reorganiza a 1
+                        jugador.progreso = 1;
+                        // Se guarda el puntaje y el tiempo por si no termina el 2do nivel
+                        jugador.mejorTiempo = seconds;
+                        jugador.ultPuntaje = puntaje;
+                        if (jugador.puntajeMax < puntaje) {
+                            jugador.puntajeMax = puntaje;
+                        }
+                    }
+                });
+                localStorage.setItem('usuarios', JSON.stringify(usersLocal));
+                iniciar();
+                asignarImagenesAnimales(animalesMostrados());
+            } else {
+                usersLocal.forEach(jugador => {
+                    // Busca al jugador actual y le asigna el progreso 1
+                    if (jugador.usuario == player.usuario) {
+                        // Si el progreso es diferente de 1  (0 o 2) significa que ya paso el primer nivel, se reorganiza a 1
+                        jugador.progreso = 2;
+                        jugador.mejorTiempo = seconds;
+                        jugador.ultPuntaje = puntaje;
+                        if (jugador.puntajeMax < puntaje) {
+                            jugador.puntajeMax = puntaje;
+                        }
+                    }
+                });
+                localStorage.setItem('usuarios', JSON.stringify(usersLocal));
+
+            }
+        }
     } else {
         // El animal no se arrastró a la casa correcta
         elemento.style.position = 'initial'; // Regresar a la posición original
         puntaje -= Math.round(Math.floor(Math.random() * 100) * seconds);
-        //TODO Llamar al sonido de que es incorrecta
-
+        // Se resta puntaje por error
+        var audio = new Audio("audio/error.mp3");
+        audio.play();
+        // Se ejecuta el audio indicando que es el animal incorrecto
     }
-    // console.log("hola perra");
     divPuntaje.innerHTML = puntaje;
 }
 function startTimer(timerElement) {
@@ -159,7 +221,7 @@ function startTimer(timerElement) {
 // Este windows.onload también se hará para las siguientes ejecuciones de la función iniciar
 window.onload = function () {
     iniciar();
-    asignarImagenesAnimales(primerosTres);
+    asignarImagenesAnimales(animalesMostrados());
 
     // Iniciar el cronómetro
     timerElement = document.getElementById('timer');
